77168bb0fd8ac9a6ab662f38a8b85e8349b76269
leeeeegun <leeeeegun@naver.com>
2025-07-23 17:47:28 +0900

    feat: [이의건] FileService MultipartFile 저장 + DB 반영 [RBA-BACK-6]

---
+++ backend/src/main/java/com/codingrecipe/backend/service/FileService.java
@@ -0,0 +1,36 @@
+package com.codingrecipe.backend.service;
+
+import com.codingrecipe.backend.entity.BoardEntity;
+import com.codingrecipe.backend.entity.FileEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class FileService {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    private final FileRepository fileRepository;
+
+    public FileEntity saveFile(MultipartFile multipartFile, BoardEntity boardEntity) throws IOException {
+        String originalFileName = multipartFile.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString();
+        String storedFileName = uuid + "_" + originalFileName;
+        String filePath = fileDir + storedFileName;
+
+        FileEntity fileEntity = new FileEntity();
+        fileEntity.setOriginalFileName(originalFileName);
+        fileEntity.setStoredFileName(storedFileName);
+        fileEntity.setFilePath(filePath);
+        fileEntity.setBoard(boardEntity);
+
+        return fileRepository.save(fileEntity);
+    }
+}
