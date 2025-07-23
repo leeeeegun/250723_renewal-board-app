6cea11a00f01455b8a91e527daa00cd04ee08d7a
leeeeegun <leeeeegun@naver.com>
2025-07-23 16:24:42 +0900

    feat: [이의건] BoardRepository 초기 구성 [RBA-1]

---
+++ backend/src/main/java/com/codingrecipe/backend/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.codingrecipe.backend.repository;
+
+import com.codingrecipe.backend.entity.BoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+}
