ec09ebfcea3a0e7644e44ab846b2c4d5e59e0c6c
leeeeegun <leeeeegun@naver.com>
2025-07-29 11:22:26 +0900

    feat: [이의건] footer 기본 설정 [RBA-FRONT-3]

---
+++ frontend/src/components/common/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../assets/styles/Footer.css';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="app-footer">
+            <p>&copy; 2025 익명 게시판. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer;
